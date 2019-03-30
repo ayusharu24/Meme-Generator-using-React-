ab065134566f7a3ccba3b6aa060ee0f536cb956c
ayusharu24 <48948004+ayusharu24@users.noreply.github.com>
2019-03-30 13:38:47 +0530

    Add files via upload

---
+++ MyAppMeme.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./components/Header1"
+import MemeGenerator from "./components/MemeGenerator"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <MemeGenerator />
+        </div>
+    )
+}
+
+export default App
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyAppM from './MyAppMeme';
+import style from './style.css'
+ReactDOM.render(<MyAppM />,document.querySelector("#root"));
